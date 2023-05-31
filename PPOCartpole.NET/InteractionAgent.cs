2c0f953ef901de30a20c7b718fd340dc1ae51443
samuelcaldas <samuel.caldas@gmail.com>
2023-05-31 08:37:33 -0300

    Split program and InteractionAgent

---
+++ PPOCartpole.NET/InteractionAgent.cs
@@ -0,0 +1,99 @@
+﻿using GymSharp;
+using PPO.NET;
+using PPOCartpole.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PPOCartpole.NET
+{
+    public class InteractionAgent
+    {
+        private Env env;
+        private PPOBinding ppo;
+        private int stepsPerEpoch;
+        private int epochs;
+        private int trainPolicyIterations;
+        private int trainValueIterations;
+        private double targetKl;
+        private int[] hiddenSizes;
+
+        public InteractionAgent(Env env, PPOBinding ppo, int stepsPerEpoch, int epochs, int trainPolicyIterations, int trainValueIterations, double targetKl, int[] hiddenSizes)
+        {
+            this.env = env;
+            this.ppo = ppo;
+            this.stepsPerEpoch = stepsPerEpoch;
+            this.epochs = epochs;
+            this.trainPolicyIterations = trainPolicyIterations;
+            this.trainValueIterations = trainValueIterations;
+            this.targetKl = targetKl;
+            this.hiddenSizes = hiddenSizes;
+        }
+
+        public void Train()
+        {
+            double[] observation = env.Reset();
+            double episodeReturn = 0;
+            int episodeLength = 0;
+
+            for (int epoch = 0; epoch < epochs; epoch++)
+            {
+                double sumReturn = 0;
+                double sumLength = 0;
+                int numEpisodes = 0;
+
+                for (int t = 0; t < stepsPerEpoch; t++)
+                {
+                    var sampledAction = ppo.SampleAction(observation);
+                    double[][] logits = sampledAction.Item1;
+                    int action = sampledAction.Item2;
+
+                    (double[] observationNew, double reward, bool done) = env.Step(action);
+                    episodeReturn += reward;
+                    episodeLength += 1;
+
+                    double valueT = ppo.Critic(observation);
+                    double logProbabilityT = ppo.LogProbabilities(logits, action);
+
+                    ppo.buffer.Store(observation, action, reward, valueT, logProbabilityT);
+
+                    observation = observationNew;
+
+                    bool terminal = done || (t == stepsPerEpoch - 1);
+                    if (terminal)
+                    {
+                        double lastValue = done ? 0 : ppo.Critic(observation);
+                        ppo.buffer.FinishTrajectory(lastValue);
+                        sumReturn += episodeReturn;
+                        sumLength += episodeLength;
+                        numEpisodes += 1;
+                        observation = env.Reset();
+                        episodeReturn = 0;
+                        episodeLength = 0;
+                    }
+                }
+
+                var (observationBuffer, actionBuffer, advantageBuffer, returnBuffer, logProbabilityBuffer) = ppo.buffer.Get();
+
+                double kl = 0;
+                for (int i = 0; i < trainPolicyIterations; i++)
+                {
+                    kl = ppo.TrainPolicy(observationBuffer, actionBuffer, logProbabilityBuffer, advantageBuffer);
+                    if (kl > 1.5 * targetKl)
+                    {
+                        Console.WriteLine($"Early stopping at iteration {i} due to reaching max kl: {kl:F2}");
+                        break;
+                    }
+                }
+
+                for (int i = 0; i < trainValueIterations; i++)
+                {
+                    ppo.TrainValueFunction(observationBuffer, returnBuffer);
+                }
+            }
+        }
+    }
+}
--- PPOCartpole.NET/Program.cs
+++ PPOCartpole.NET/Program.cs
@@ -6,18 +6,7 @@ namespace PPOCartpole.NET
 {
     internal class Program
     {
-        private static dynamic step_state;
-        private static double reward;
-        private static double kl;
-
         static void Main(string[] args)
-        {
-            TestPPOEnv();
-
-            Console.ReadKey();
-        }
-
-        private static void TestPPOEnv()
         {
             /// <summary>
             /// Hyperparameters
@@ -41,7 +30,7 @@ namespace PPOCartpole.NET
             // Initialize the environment and get the dimensionality of the
             // observation space and the number of possible actions
             Env env = new Env("CartPole-v1");
-            Console.Clear();
+            //Console.Clear();
             int observationDimensions = env.ObservationDimensions;
             int numActions = env.NumActions;
 
@@ -56,106 +45,17 @@ namespace PPOCartpole.NET
                                             gamma: (double)0.99,
                                             lam: (double)0.95);
 
-            /// <summary>
-            /// Load the model
-            /// </summary>
-            //ppo.Load("my_model");
+            InteractionAgent agent = new InteractionAgent(env,
+                                                          ppo,
+                                                          stepsPerEpoch,
+                                                          epochs,
+                                                          trainPolicyIterations,
+                                                          trainValueIterations,
+                                                          targetKl,
+                                                          hiddenSizes);
+            agent.Train();
 
-            // Initialize the observation, episode return and episode length
-            double[] observation = env.Reset();
-            double episodeReturn = 0;
-            int episodeLength = 0;
-
-            /// <summary>
-            /// Train
-            /// </summary>
-            // Iterate over the number of epochs
-            for (int epoch = 0; epoch < epochs; epoch++)
-            {
-                // Initialize the sum of the returns, lengths and number of episodes for each epoch
-                double sumReturn = 0;
-                double sumLength = 0;
-                int numEpisodes = 0;
-
-                // Iterate over the steps of each epoch
-                for (int t = 0; t < stepsPerEpoch; t++)
-                {
-                    if (render)
-                    {
-                        env.Render();
-                    }
-
-                    // Get the logits, action, and take one step in the environment
-                    //observation = observation.reshape(1, -1);
-
-                    var sampledAction = ppo.SampleAction(observation);
-                    double[][] logits = sampledAction.Item1;
-                    int action = sampledAction.Item2;
-                    (double[] observationNew, double reward, bool done) = env.Step(action);
-                    episodeReturn += reward;
-                    episodeLength += 1;
-
-                    // Get the value and log-probability of the action
-                    double valueT = ppo.Critic(observation);
-                    double logProbabilityT = ppo.LogProbabilities(logits, action);
-
-                    // Store obs, act, rew, v_t, logp_pi_t
-                    ppo.buffer.Store(observation, action, reward, valueT, logProbabilityT);
-
-                    // Update the observation
-                    observation = observationNew;
-
-                    // Finish trajectory if reached to a terminal state
-                    bool terminal = done;
-                    if (terminal || (t == stepsPerEpoch - 1))
-                    {
-                        double lastValue = done ? 0 : ppo.Critic(observation);
-                        ppo.buffer.FinishTrajectory(lastValue);
-                        sumReturn += episodeReturn;
-                        sumLength += episodeLength;
-                        numEpisodes += 1;
-                        observation = env.Reset();
-                        episodeReturn = 0;
-                        episodeLength = 0;
-                    }
-                }
-
-                // Get values from the buffer
-                var (observationBuffer,
-                    actionBuffer,
-                    advantageBuffer,
-                    returnBuffer,
-                    logProbabilityBuffer) = ppo.buffer.Get();
-
-                // Update the policy and implement early stopping using KL divergence
-                double kl = 0;
-                for (int i = 0; i < trainPolicyIterations; i++)
-                {
-                    kl = ppo.TrainPolicy(observationBuffer, actionBuffer, logProbabilityBuffer, advantageBuffer);
-                    if (kl > 1.5 * targetKl)
-                    {
-                        Console.WriteLine($"Early stopping at iteration {i} due to reaching max kl: {kl:F2}");
-                        break;
-                    }
-                }
-
-                // Update the value function
-                for (int i = 0; i < trainValueIterations; i++)
-                {
-                    ppo.TrainValueFunction(observationBuffer, returnBuffer);
-                }
-
-                // Print mean return and length for each epoch
-                Console.WriteLine($"Epoch: {epoch + 1}. " +
-                                  $"Mean Return: {sumReturn / numEpisodes:F2}. " +
-                                  $"Mean Length: {sumLength / numEpisodes:F2}. " +
-                                  $"KL Divergence: {kl:F2}");
-            }
-
-            /// <summary>
-            /// Save the model
-            /// </summary>
-            //ppo.Save("my_model");
+            Console.ReadKey();
         }
     }
 }
